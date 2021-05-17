481499102778047aefe88b1082dc1da58a94ce6b
shiqiwang <shiqi.wang@live.com>
2021-05-18 07:41:55 +0800

    提取模板

---
+++ data.js
@@ -0,0 +1,11 @@
+module.exports.data = {
+  'qmkx.com': {
+    keywords: '全民科学,全民快讯,启蒙科学,亲妈克星,亲妹空虚',
+    title: 'QMKX.com',
+    letters: ['Q', 'M', 'K', 'X'],
+    domain: 'qmkx.com',
+    keywordsOne:
+      '全民,全面,启蒙,奇妙,球迷,千米,清明,巧妙,亲妈,亲妹,取名,启明,骑马,窍门',
+    keywordsTwo: '科学,快讯,快线,克星,开心,凯旋,狂想,狂笑,狂喜,空虚',
+  },
+};
--- main.js
+++ main.js
@@ -2,8 +2,15 @@ const Path = require('path');
 
 const express = require('express');
 
+const expressHandlebars = require('express-handlebars');
+
+const data = require('./data');
+
 const app = express();
 const port = 80;
+
+app.engine('handlebars', expressHandlebars());
+app.set('view engine', 'handlebars');
 
 app.use(express.static('static'));
 
@@ -11,6 +18,11 @@ app.get('/', (req, res) => {
   let domain = req.hostname;
 
   domain = domain.replace(/^www\./, '');
+
+  if (domain === 'qmkx.com') {
+    res.render('quad-word-domain', data.data[domain]);
+    return;
+  }
 
   res.sendFile(Path.join(__dirname, 'static', `${domain}.html`));
 });
